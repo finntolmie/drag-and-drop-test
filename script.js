b808ba754417f862e821373385fcf96a5d4d47eb
finntolmie <50947941+finntolmie@users.noreply.github.com>
2023-06-05 12:33:26 +1000

    small refactor and style

--- script.js
+++ script.js
@@ -1,33 +1,20 @@
 let item = null;
+let itemBounds = null;
+let parentBounds = null;
 
-function clampBounds(element, xPos, yPos) {
-	let elementBounds = element.getBoundingClientRect();
-	let itemBounds = item.getBoundingClientRect();
-	return [
-		Math.max(
-			-50,
-			Math.min(
-				((xPos - itemBounds.width / 2 - elementBounds.left) /
-					elementBounds.width) *
-					800,
-				750
-			)
-		),
-		Math.max(
-			-50,
-			Math.min(
-				((yPos - itemBounds.height / 2 - elementBounds.top) /
-					elementBounds.height) *
-					800,
-				750
-			)
-		),
-	];
+function clamp(number, min, max) {
+	return Math.max(min, Math.min(number, max));
+}
+
+function translateSquare(element, dx, dy) {
+	element.style.transform = `translate(${dx}%, ${dy}%)`;
 }
 
 function onGrab(e) {
 	if (e.target.classList.contains("draggable")) {
 		item = e.target;
+		itemBounds = item.getBoundingClientRect();
+		parentBounds = item.parentElement.getBoundingClientRect();
 		item.ondragstart = function () {
 			return false;
 		};
@@ -39,44 +26,59 @@ function onGrab(e) {
 
 function onDrag(e) {
 	if (item) {
-		let mouseX, mouseY;
-		[mouseX, mouseY] = clampBounds(item.parentElement, e.clientX, e.clientY);
-		let dx = mouseX;
-		let dy = mouseY;
-		item.style.transform = `translate(${dx}%, ${dy}%)`;
+		let dx = clamp(
+			((e.clientX - itemBounds.width / 2 - parentBounds.left) /
+				parentBounds.width) *
+				800,
+			-50,
+			750
+		);
+		let dy = clamp(
+			((e.clientY - itemBounds.height / 2 - parentBounds.top) /
+				parentBounds.height) *
+				800,
+			-50,
+			750
+		);
+		translateSquare(item, dx, dy);
 	}
 }
 
 function onLetGo(e) {
-	let parentBounds = item.parentElement.getBoundingClientRect();
-	let row = Math.max(
+	// translate square snap to grid
+	let row = clamp(
+		Math.floor((e.clientY - parentBounds.top) / (parentBounds.height / 8)),
 		0,
-		Math.min(
-			Math.floor((e.clientY - parentBounds.top) / (parentBounds.height / 8)),
-			7
-		)
+		7
 	);
-	let col = Math.max(
+	let col = clamp(
+		Math.floor((e.clientX - parentBounds.left) / (parentBounds.width / 8)),
 		0,
-		Math.min(
-			Math.floor((e.clientX - parentBounds.left) / (parentBounds.width / 8)),
-			7
-		)
+		7
 	);
-	item.style.transform = `translate(${col * 100}%, ${row * 100}%)`;
+	translateSquare(item, col * 100, row * 100);
+
+	// remove old square class
 	[].some.call(item.classList, (c) => {
 		if (/square-.*/.test(c)) {
 			item.classList.remove(c);
 		}
 	});
+
+	// remove element on new square and add new square class
 	let newSquare = `square-${col + 1}${8 - row}`;
 	document.querySelectorAll(`.${newSquare}`).forEach((square) => {
 		square.remove();
 	});
 	item.classList.remove("dragging");
 	item.classList.add(newSquare);
+
+	// cleanup
 	window.removeEventListener("mousemove", onDrag);
 	window.removeEventListener("mouseup", onLetGo);
+	item = null;
+	itemBounds = null;
+	parentBounds = null;
 }
 
 window.addEventListener("mousedown", onGrab);
