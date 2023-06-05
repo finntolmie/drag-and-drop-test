2c3ed2866e2909d13ed9db1d5154dcdcdc9349fd
finntolmie <50947941+finntolmie@users.noreply.github.com>
2023-06-05 13:23:13 +1000

    hovered square svg credit chess.com

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 let item = null;
 let itemBounds = null;
-let parentBounds = null;
+let parent = document.querySelector(".bounds");
+let parentBounds = parent.getBoundingClientRect();
+let hover = document.createElement("div");
+hover.classList.add("hover");
+hover.style.visibility = "hidden";
+hover.innerHTML =
+	'<svg viewBox="0 0 100 100"><rect x="0" y="0" width="100" height="100" stroke="rgba(255, 255, 255, 0.65)" stroke-width="10" fill="none"></rect></svg>';
+parent.insertBefore(hover, parent.firstChild);
 
 function clamp(number, min, max) {
 	return Math.max(min, Math.min(number, max));
@@ -14,10 +21,14 @@ function onGrab(e) {
 	if (e.button == 0 && e.target.classList.contains("draggable")) {
 		item = e.target;
 		itemBounds = item.getBoundingClientRect();
-		parentBounds = item.parentElement.getBoundingClientRect();
+		parent = item.parentElement;
+		parentBounds = parent.getBoundingClientRect();
 		item.ondragstart = function () {
 			return false;
 		};
+		hover.style.visibility = "";
+		hover.style.transform = item.style.transform;
+
 		item.classList.add("dragging");
 		window.addEventListener("mousemove", onDrag);
 		window.addEventListener("mouseup", onLetGo);
@@ -40,6 +51,17 @@ function onDrag(e) {
 			-50,
 			750
 		);
+		let row = clamp(
+			Math.floor((e.clientY - parentBounds.top) / (parentBounds.height / 8)),
+			0,
+			7
+		);
+		let col = clamp(
+			Math.floor((e.clientX - parentBounds.left) / (parentBounds.width / 8)),
+			0,
+			7
+		);
+		translateItem(hover, col * 100, row * 100);
 		translateItem(item, dx, dy);
 	}
 }
@@ -72,6 +94,8 @@ function onLetGo(e) {
 	});
 	item.classList.remove("dragging");
 	item.classList.add(newSquare);
+
+	hover.style.visibility = "";
 
 	// cleanup
 	window.removeEventListener("mousemove", onDrag);
